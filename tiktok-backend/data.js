a7531db882835cbd4263e9d3c0ea0782f6dfec5a
Sanjukumar8238 <sanjuphogat8238@gmail.com>
2022-12-15 12:29:33 +0530

    backend added

---
+++ tiktok-backend/data.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    url: "https://www.w3schools.com/html/mov_bbb.mp4",
+    channel: "sanju",
+    description: "too  fndjn afd fa",
+    song: "fkasjf jeajf df nanjk ajl",
+    likes: "123",
+    messages: "21",
+    shares: "43",
+  },
+  {
+    url: "https://www.w3schools.com/html/mov_bbb.mp4",
+    channel: "sfkdsu",
+    description: "tdksafd fa",
+    song: "fkdfskfa df nanjk ajl",
+    likes: "18",
+    messages: "21",
+    shares: "3",
+  },
+];
+
+export default data;
